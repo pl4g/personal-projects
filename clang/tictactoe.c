76d4219dfd80b74eb429dd3341aff5e6e44030d6
pl4g <viniciuslucianocardoso@gmail.com>
2023-09-06 21:16:00 -0300

    Did some more C coding :3

--- clang/tictactoe.c
+++ clang/tictactoe.c
@@ -5,45 +5,44 @@
 
 #define LEN(arr) ((int) (sizeof (arr) / sizeof (arr)[0]))
 
-enum GameState {
-  isPlaying = 0,
-  player1Won = 1,
-  player2Won = 2,
-};
+typedef enum {
+  isPlaying,
+  player1Won,
+  player2Won,
+} GameState;
 
-struct Player {
+typedef enum {
+  none,
+  X,
+  O
+} Players;
+
+typedef struct {
   char playerChar;
-};
+} Player;
 
-enum Players {
-  X = 1,
-  O = 2
-};
+typedef struct {
+  Players cells[3][3];
+} GameTable;
 
-struct GameTable {
-  int cells[3][3];
-};
+GameTable invert3by3Array(GameTable *oldGameTable);
 
-struct GameTable invert3by3Array(struct GameTable *oldGameTable);
+typedef struct {
+  GameState gameState;
 
-struct Game {
-  enum GameState gameState;
+  Player players[3]; //3 players because player 0 is the not set;
+  Players currentPlayer;
 
-  struct Player players[3];
-  enum Players currentPlayer;
+  GameTable gameTable;
+} Game;
 
-  struct GameTable gameTable;
-};
+void printGameTable(Game* game);
+void getGameState(Game* game);
 
-void printGameTable(struct Game* game);
-void getGameState(struct Game* game);
-
-bool compareIntArray(int array[]);
-
-size_t getSizeOfArray(int array[]);
+bool checkWinCondition(Players array[]);
 
 int main() {
-  struct Game game = {
+  Game game = {
     .gameTable.cells = {0,0,0,0,0,0,0,0,0},
     .gameState = isPlaying,
     .currentPlayer = X
@@ -54,14 +53,13 @@ int main() {
   strcpy(&game.players[2].playerChar, "O");
 
   while (game.gameState == isPlaying) {
-    printf("\n");
     printGameTable(&game);
-    printf("\n");
 
     char move[6];
 
     printf("Player %d (%c), choose a cell to play (row,collumn): ", game.currentPlayer, game.players[game.currentPlayer].playerChar);
     fgets(move, 6, stdin);
+    printf("\n");
 
     move[strlen(move) - 1] = '\0';
 
@@ -73,15 +71,14 @@ int main() {
       continue;
     }
 
-    printf("You picked the cell at (%d, %d),", row, col);
-
     game.gameTable.cells[row][col] = game.currentPlayer;
 
     getGameState(&game);
 
-    printf("Game State: %d", game.gameState);
-
     if (game.gameState != isPlaying) {
+      printGameTable(&game);
+
+      printf("Player %d (%c) Won!\n", game.currentPlayer, game.players[game.currentPlayer].playerChar);
       break;
     }
 
@@ -91,25 +88,25 @@ int main() {
   return 0;
 }
 
-void getGameState(struct Game* game){
+void getGameState(Game* game){
 
   if (game->gameState != isPlaying) return;
 
   //check for a win in each row.
-  for (int row; row <= 2; row++) {
-    if (compareIntArray(game->gameTable.cells[row])) {
-      game->gameState = game->gameTable.cells[row][0];
+  for (int row = 0; row <= 2; row++) {
+    if (checkWinCondition(game->gameTable.cells[row])) {
+      game->gameState = (GameState)game->gameTable.cells[row][0];
     }
   }
 
   if (game->gameState != isPlaying) return;
 
   //check for a win in each column.
-  for (int col; col <= 2; col++) {
-    struct GameTable inverseGameTable = invert3by3Array(&game->gameTable);
+  for (int col = 0; col <= 2; col++) {
+    GameTable inverseGameTable = invert3by3Array(&game->gameTable);
 
-    if (compareIntArray(inverseGameTable.cells[col])) {
-      game->gameState = inverseGameTable.cells[col][0];
+    if (checkWinCondition(inverseGameTable.cells[col])) {
+      game->gameState = (GameState)inverseGameTable.cells[col][0];
     }
   }
 
@@ -121,11 +118,14 @@ void getGameState(struct Game* game){
       (game->gameTable.cells[1][1] == game->gameTable.cells[0][2] &&
       game->gameTable.cells[1][1] == game->gameTable.cells[2][0]))
   {
-      game->gameState = game->gameTable.cells[1][1];
+      game->gameState = (GameState)game->gameTable.cells[1][1];
   }
 }
 
-void printGameTable(struct Game* game) {
+void printGameTable(Game* game) {
+
+  printf("\n");
+
   for (int row = 0; row <= 2; row++) {
     for (int col = 0; col <= 2; col++) {
       printf(" %c ", game->players[game->gameTable.cells[row][col]].playerChar);
@@ -135,31 +135,32 @@ void printGameTable(struct Game* game) {
 
     if (row < 2) printf("\n---|---|---\n");
   }
+
+  printf("\n");
 }
 
-bool compareIntArray(int array[]){
+bool checkWinCondition(Players array[]){
   int prev = array[0];
 
-  for (int i = 0; i <= getSizeOfArray(array); i++) {
-    if (array[i] != prev) return false;
+  for (int i = 0; i <= 2; i++) {
+    if (array[i] != prev || array[i] == none) { 
+      return false;
+    }
+
     prev = array[i];
   }
 
   return true;
 }
 
-struct GameTable invert3by3Array(struct GameTable *oldGameTable) {
-  struct GameTable newGameTable;
+GameTable invert3by3Array(GameTable *oldGameTable) {
+  GameTable newGameTable;
 
-  for (int row = 0; row <= LEN(oldGameTable->cells); row++) {
-    for (int col = 0; col <= LEN(oldGameTable->cells[0]); col++) {
+  for (int row = 0; row <= LEN(oldGameTable->cells) - 1; ++row) {
+    for (int col = 0; col <= LEN(oldGameTable->cells[0]) - 1; ++col) {
       newGameTable.cells[row][col] = oldGameTable->cells[col][row];
     }
   }
 
   return newGameTable;
-}
-
-size_t getSizeOfArray(int array[]){
-  return sizeof(&array) / sizeof(array[0]);
 }
